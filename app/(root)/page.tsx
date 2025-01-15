63a88edcf66a6c6197abcb625573101ebd131400
Chirag Singh <chiraaajsingh7600@gmail.com>
2025-01-15 17:29:08 +0530

    add startup cards placeholder

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,30 @@
-import SearchForm from "../components/SearchForm";
+import StartupCard from "@/components/StartupCard";
+import SearchForm from "../../components/SearchForm";
 
-export default function Home({
+export default async function Home({
   searchParams,
 }: {
   searchParams: Promise<{ query: string }>;
 }) {
+  const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createAt: new Date(),
+      views: 55,
+      author: {
+        _id: 1,
+        name:"Chirag"
+      },
+      _id: 1,
+      desciption: "This is a description",
+      image:
+        "https://png.pngtree.com/thumb_back/fh260/background/20230408/pngtree-robot-white-cute-robot-blue-light-background-image_2199825.jpg",
+      title: " We Robots",
+      category: "Robots",
+    },
+  ];
+
   return (
     <>
       <section className="pink_container">
@@ -16,7 +36,22 @@ export default function Home({
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
           Comprtitions.
         </p>
-        <SearchForm />
+        <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for ${query}` : `All Startups`}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (posts.map((post:StartupCardType)=>(
+            <>  
+              <StartupCard key={post?._id} post={post}/>
+            </>
+          ))):(<>
+            <p className="no-results">No startups found</p>
+          </>)}
+        </ul>
       </section>
     </>
   );
