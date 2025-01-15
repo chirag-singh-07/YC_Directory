63a47a4089da839d147f894f05f1794d764f9946
Chirag Singh <chiraaajsingh7600@gmail.com>
2025-01-15 16:24:36 +0530

    add auth js

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,53 @@
+import { auth, signIn, signOut } from "@/auth";
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = async () => {
+  const session = await auth();
+
+  return (
+    <div className="px-5 py-2 bg-white shadow-sm font-work-sans ">
+      <nav className="flex items-center justify-between">
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" width={144} height={30} />
+        </Link>
+
+        <div className="flex items-center gap-5 text-black">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut();
+                }}
+              >
+                <button type="submit">Logout</button>
+              </form>
+
+              <Link href={`/profile/${session?.user?.id}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <>
+              <form
+                action={async () => {
+                  "use server";
+                  await signIn("github");
+                }}
+              >
+                <button type="submit">Sign in with GitHub</button>
+              </form>
+            </>
+          )}
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
